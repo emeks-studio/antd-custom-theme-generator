aaa0d6c27e20384d080eae714cb9972095ac4c5a
tortugo <turco807@gmail.com>
2020-06-27 21:11:17 -0300

    Prepare the repo to be an open source library
    - Add licence
    - Modify project name
    - Modify code in order to make it executable by command

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const { exec } = require('child_process');
+const path = require('path');
+
+console.log('Initial params:', process.argv, process.cwd(), __dirname);
+
+const scriptVariables = {
+  customThemeFilePath: path.join(process.cwd(), process.argv[2] || './custom-theme.less'),
+  generatedThemeFilePath: path.join(process.cwd(), process.argv[3] || './custom-theme.css'),
+  antdLibraryPath: path.join(process.cwd(), process.argv[4] || './node_modules/antd')
+};
+console.log('Script variables:', scriptVariables);
+
+const newThemContent = `
+  @import url("${scriptVariables.antdLibraryPath}/lib/style/themes/default.less");
+  @import url("${scriptVariables.antdLibraryPath}/dist/antd.less");
+  @import url("${scriptVariables.customThemeFilePath}");
+`;
+console.debug('About to generate "/tmp/generated-theme.less" tmp file:', newThemContent)
+
+exec(`echo "${newThemContent}" > /tmp/generated-theme.less`, { cwd: process.cwd() }, (error1, stdout, stderr) => {
+  if(!error1) {
+    console.debug(`Dynamic generation: "/tmp/generated-theme.less" tmp file was successfully generated`);
+    exec(
+      `node_modules/less/bin/lessc --js /tmp/generated-theme.less ${scriptVariables.generatedThemeFilePath}`,
+      { cwd: __dirname },
+      (error2, stdout, stderr) => {
+        if(!error2) {
+          console.log(`Finally: "${scriptVariables.generatedThemeFilePath}" was successfully generated`);
+        } else {
+          console.error(error2);
+        }
+      }
+    );
+  } else {
+    console.error(error1);
+  };
+});
