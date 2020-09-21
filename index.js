d8498a781664ba6c710aae771f67fb82c3b88f20
Mehdi Rezaei <mehdiraized@gmail.com>
2020-09-21 03:55:01 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const { exec } = require('child_process');
 const path = require('path');
-const debug = process.argv[2] === 'verbose';
+const debug = process.argv[2] !== '';
 
 const scriptParams = {
   'process.argv': process.argv,
